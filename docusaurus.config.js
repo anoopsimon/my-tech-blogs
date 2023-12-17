5b05cf323e4e7e4d0d4b21437e1bab7f5f34b8ca
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2023-12-17 20:54:50 +1100

    updated base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://anoopsimon.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-tech-blogs/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'anoopsimon', // Usually your GitHub org/user name.
